301d01e1e659d2698b788aeeef61eb13e5f14813
Milosz Dura <miloszdura@gmail.com>
2017-11-17 16:54:54 +0000

    initial commit

---
+++ RomanNumerals.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumerals.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsTests
+    {
+        private RomanNumeralsConverter _subject;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _subject = new RomanNumeralsConverter();
+        }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(568, "DLXVIII")]
+        public void ThenTheCorrectRomanNumeralIsReturned(int decimalNumber, string expectedValue)
+        {
+            var result = _subject.Convert(decimalNumber);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumeralsConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralsConverter
+    {
+        private readonly Dictionary<int, string> _romanNumerals;
+
+        public RomanNumeralsConverter()
+        {
+            _romanNumerals = new Dictionary<int, string>
+            {
+                { 1000, "M" },
+                { 500, "D" },
+                { 100, "C" },
+                { 50, "L" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" }
+            };
+        }
+
+        public string Convert(int number)
+        {
+            var response = "";
+
+            foreach (var decimalNumber in _romanNumerals.Keys)
+            {
+                while (number >= decimalNumber)
+                {
+                    response += _romanNumerals[decimalNumber];
+                    number -= decimalNumber;
+                }
+            }
+
+            return response;
+        }
+    }
+}
